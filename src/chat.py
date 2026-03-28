76a783fb46ba08475237c4aa6b9a04ef5bec56e7
Shinya <aitech.guru365@gmail.com>
2026-03-27 20:56:21 -0400

    Initial AI chatbot(LSTM base)

--- src/chat.py
+++ src/chat.py
@@ -1,93 +1,37 @@
-# nltk -> Natural Language Toolkit, a Python library for processing human language.
-# json -> To read or write JSON files (commonly used to store chatbot intents or text data).
-# random -> For random selections (like picking a random response).
-# numpy(np) -> For numerical operations, arrays, and computations.
-
-# PorterStemmer -> A tool to reduce words to their root form(stemming). Example: running -> run, happily -> happi
-# word_tokenize -> A function to split a sentence into words or tokens. Example: "Hello World!" -> ["Hello", "world", "!"]
-
-# [stemmer.stem(word) for word in words] -> List comprehension: For each word in the tokenized list, apply the stemmer. Example: ["running", "jumps", "happily"] -> ["run", "jump", "happi"]
-# preprocess(sentence): Return the stemmed tokens. The function outputs a list of simplified words. preprocess("I am running happily!") -> Output: ['i', 'am', 'run', 'happi', '!']
-
-import nltk
-import json
+import torch
 import random
-import numpy as np
+import json
 
-from nltk.stem import PorterStemmer
-from nltk.tokenize import word_tokenize
+from model import NeuralNet
+from nltk_utils import bag_of_words, tokenize
 
-stemmer = PorterStemmer()
+data = torch.load("../model/chatbot_model.pth")
 
-# Return the stemmed tokens. The function outputs a list of simplified words. 
-def preprocess(sentence):
-    words = word_tokenize(sentence.lower())
-    return [stemmer.stem(word) for word in words]
+model = NeuralNet(data["input_size"], data["hidden_size"], data["output_size"])
+model.load_state_dict(data["model_state"])
+model.eval()
 
-# The function will convert a sentence into a bag-of words vector (Numbers). Example: tokenized_sentence = ["Hello", "you"], words = ["hello", "bye", "thanks", "you"] -> bag_of_words(tokenized_sentence, words) # Output: [1.0, 0.0, 0.0, 1.0]
-def bag_of_words(tokenized_sentence, words):
-    sentence_words = [stemmer.stem(word) for word in tokenized_sentence]
-    bag = np.zeros(len(words), dtype=np.float32)
+all_words = data["all_words"]
+tags = data["tags"]
 
-    for idx, w in enumerate(words):
-        if w in sentence_words:
-            bag[idx] = 1
+with open("../data/intents.json") as f:
+    intents = json.load(f)
+
+print("Chatbot ready! (type 'quit')")
+
+while True:
+    sentence = input("You: ")
+    if sentence == "quit":
+        break
+
+    X = bag_of_words(tokenize(sentence), all_words)
+    X = torch.from_numpy(X)
+
+    output = model(X)
+    _, predicted = torch.max(output, dim=0)
+
+    tag = tags[predicted.item()]
     
-    return bag
-
-# Prepare Training Data
-with open("../data/intents.json") as file:
-    data = json.load(file)
-
-all_words = []
-tags = []
-xy = [] # (words, tag)
-
-for intent in data["intents"]:
-    tag = intent["tag"]
-    tags.append(tag)
-
-    for pattern in intent["patterns"]:
-        w = preprocess(pattern)
-        all_words.extend(w)
-        xy.append((w, tag))
-
-all_words = sorted(set(all_words))
-tags = sorted(set(tags))
-
-X_train = []
-y_train = []
-
-for(pattern_sentence, tag) in xy:
-    bag = bag_of_words(pattern_sentence, all_words)
-    X_train.append(bag)
-    y_train.append(tags.index(tag))
-
-X_train = np.array(X_train)
-y_train = np.array(y_train)
-
-#####
-from sklearn.linear_model import LogisticRegression
-
-model = LogisticRegression()
-model.fit(X_train, y_train)
-
-def chatbot():
-    print("Chatbot is running! (type 'quit' to stop)")
-
-    while True:
-        sentence = input("You: ")
-        if sentence == "quit":
-            break
-        
-        tokens = preprocess(sentence)
-        bag = bag_of_words(tokens, all_words)
-
-        prediction = model.predict([bag])[0]
-        tag = tags[prediction]
-
-        for intent in data["intents"]:
-            if intent["tag"] == tag:
-                print("Bot:", random.choice(intent["responses"]))
-
-chatbot()
+    for intent in intents["intents"]:
+        if tag == intent["tag"]:
+            print("Bot:", random.choice(intent["responses"]))
